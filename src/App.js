67601ea5e35039551b80d9a08124b93cf22f12d1
jdc-cunningham <jcun4128@gmail.com>
2019-09-10 18:53:05 -0500

    trying to setup base stuff, distracted, man I am rusty with React learning a different library/framework for my job at this time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
+import Header from './components/header/Header.js';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = {
+    activeApp: ""
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header props={this.activeApp} />
+      </div>
+    );
+  }
 }
 
 export default App;
